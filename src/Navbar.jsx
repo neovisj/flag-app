9598db2836fdba2482c90bd5e7a5ad0c802dae6c
Neo <neo.viiss@gmail.com>
2025-05-28 19:58:24 +0200

    Trying to fix logo problem

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-
+import logoLight from "./assets/techover-logo.png";
+import logoDark from "./assets/techover-logo-dark.png";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
   return (
@@ -11,13 +12,10 @@ const Navbar = ({ darkMode, setDarkMode }) => {
         </Link>
         <img
           className="nav-logo"
-          src={
-            darkMode
-              ? "assets/techover-logo.png"
-              : "assets/techover-logo-dark.png"
-          }
+          src={darkMode ? logoLight : logoDark}
           alt="Techover logo"
         />
+
         <div className="button-mode-container">
           <button
             onClick={() => setDarkMode((prev) => !prev)}
